54c0da4d8b39b5cd3f005c96a3e5b74735d41b7b
GustiAhdanZiyad <gustiahdan07@gmail.com>
2026-09-20 19:59:33 +0800

    Menambahkan MODUL 0

--- MODUL 1/PRAK104-2610817110018-Gusti_Ahdan_Ziyad.py
+++
@@ -1,8 +0,0 @@
-shoe_a = 400000
-shoe_b = 350000
-discount_a = int(shoe_a - (shoe_a * 0.13))
-discount_b = int(shoe_b - (shoe_b * 0.21))
-print(f"Harga sepatu A adalah {shoe_a}")
-print(f"Harga sepatu B adalah {shoe_b}")
-print(f"Sepatu A mendapat diskon 13% sehingga harganya menjadi {discount_a}")
-print(f"Sepatu B mendapat diskon 21% sehingga harganya menjadi {discount_b}")
